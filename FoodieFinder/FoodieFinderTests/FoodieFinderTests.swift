ddbcee7916324d38b30379190c0ec6297a6dc285
Nick Reichard <reich452@regis.edu>
2019-11-23 15:35:28 -0700

    Added swfitlint

--- FoodieFinder/FoodieFinderTests/FoodieFinderTests.swift
+++ FoodieFinder/FoodieFinderTests/FoodieFinderTests.swift
@@ -6,22 +6,14 @@
 //  Copyright © 2019 NickReichard. All rights reserved.
 //
 
-import XCTest
 @testable import FoodieFinder
+import XCTest
 
 class FoodieFinderTests: XCTestCase {
 
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        super.setUp()
     }
 
     func testPerformanceExample() {
---
+++ FoodieFinder/Pods/Target Support Files/Pods-FoodieFinderUITests/Pods-FoodieFinderUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_FoodieFinderUITests : NSObject
+@end
+@implementation PodsDummy_Pods_FoodieFinderUITests
+@end
