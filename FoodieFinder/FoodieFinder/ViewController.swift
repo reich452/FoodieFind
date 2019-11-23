cef9ec28370b9619afcb14f0726ee3047dd7b530
Nick Reichard <reich452@regis.edu>
2019-11-23 14:56:19 -0700

    First commit

---
+++ FoodieFinder/FoodieFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodieFinder
+//
+//  Created by Nick Reichard on 11/23/19.
+//  Copyright © 2019 NickReichard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
