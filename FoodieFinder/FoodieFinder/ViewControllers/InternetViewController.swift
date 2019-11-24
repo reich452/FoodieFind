e536d0bd3e2b0a78cfeb6023110b49bf3bb81c1b
Nick Reichard <reich452@regis.edu>
2019-11-24 13:22:09 -0700

    Set up storyboard references

---
+++ FoodieFinder/FoodieFinder/ViewControllers/InternetViewController.swift
@@ -0,0 +1,28 @@
+//
+//  InternetViewController.swift
+//  FoodieFinder
+//
+//  Created by Nick Reichard on 11/24/19.
+//  Copyright © 2019 NickReichard. All rights reserved.
+//
+
+import UIKit
+
+class InternetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FoodieFinder/FoodieFinder/ViewControllers/RestaurantViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LunchViewController.swift
+//  FoodieFinder
+//
+//  Created by Nick Reichard on 11/24/19.
+//  Copyright © 2019 NickReichard. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        
+
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+}
