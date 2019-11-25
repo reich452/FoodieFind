ab0b5e3abd47ed06a2d9d0741986d0b90279078b
Nick Reichard <reich452@regis.edu>
2019-11-25 12:25:29 -0700

    added restaurant detail cells

---
+++ FoodieFinder/FoodieFinder/Cells/RestaurantInfoCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  RestaurantInfoCollectionViewCell.swift
+//  FoodieFinder
+//
+//  Created by Nick Reichard on 11/25/19.
+//  Copyright © 2019 NickReichard. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantInfoCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var addressLabel: UILabel!
+    @IBOutlet private weak var phoneLabel: UILabel!
+    @IBOutlet private weak var twitterLabel: UILabel!
+    
+    @IBOutlet private weak var facebookLabel: UILabel!
+    @IBOutlet private weak var facebookNameLabel: UILabel!
+}
---
+++ FoodieFinder/FoodieFinder/Cells/RestaurantNameCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  RestaurantDetailCollectionViewCell.swift
+//  FoodieFinder
+//
+//  Created by Nick Reichard on 11/25/19.
+//  Copyright © 2019 NickReichard. All rights reserved.
+//
+
+import UIKit
+
+class RestaurantNameCollectionViewCell: UICollectionViewCell {
+    @IBOutlet private weak var nameLabel: UILabel!
+    
+    @IBOutlet private weak var catagoryLable: UILabel!
+}
